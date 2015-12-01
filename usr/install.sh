150677867c3c7137cea0df90c03cdfb1bc802cd1
root <root@manager.openstack.site>
2015-12-01 16:02:16 +0000

    msg

--- usr/install.sh
+++ usr/install.sh
@@ -1,3 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sudo rm -rf /opt/stack/swift/swift
 sudo cp -r /home/stack/OverEncryptionSwift/swift  /opt/stack/swift/
+sudo chmod +x /opt/stack/swift/swift/usr/command-git.sh
