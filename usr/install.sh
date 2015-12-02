e240899522f002f26cfbaada44f189c48f7058f9
AlessandroSaullo <alessandro.saullo1@gmail.com>
2015-12-02 11:30:51 +0100

    Create install.sh

---
+++ usr/install.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+sudo rm -rf /opt/stack/swift/swift
+sudo cp -r /home/stack/swift  /opt/stack/swift/
+
+sudo rm /etc/swift/proxy-server.conf
+sudo stow -t /etc/swift/ -S /home/stack/swift/usr/stow/etc/swift
+
+sudo rm /opt/stack/swift/swift.egg-info/entry_points.txt
+sudo stow -t /opt/stack/swift/swift.egg-info/ -S /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/
