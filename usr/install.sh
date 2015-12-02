1d8993729c58d07f8e4663f72450c4a3f9827d90
AlessandroSaullo <alessandro.saullo1@gmail.com>
2015-12-02 12:14:24 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -5,7 +5,7 @@ sudo rm -rf /opt/stack/swift/swift
 sudo cp -r /home/stack/swift  /opt/stack/swift/
 
 sudo rm /etc/swift/proxy-server.conf
-sudo stow -t /etc/swift/ -S /home/stack/swift/usr/stow/etc/swift
+sudo stow -t /etc/swift/ -S /opt/stack/swift/swift/usr/stow/etc/swift
 
 sudo rm /opt/stack/swift/swift.egg-info/entry_points.txt
-sudo stow -t /opt/stack/swift/swift.egg-info/ -S /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/
+sudo stow -t /opt/stack/swift/swift.egg-info/ -S /opt/stack/swift/swift/usr/stow/opt/stack/swift/swift.egg-info/
