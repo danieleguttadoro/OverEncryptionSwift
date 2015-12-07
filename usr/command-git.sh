d3adbb964d09f770b184ab2f2513a150029d0b3d
root <root@manager.openstack.site>
2015-12-07 16:30:12 +0000

    changed command-git.sh

--- usr/command-git.sh
+++ usr/command-git.sh
@@ -4,4 +4,4 @@ echo "Insert commit message"
 read msg
 sudo git commit -m msg
 sudo git remote add origin https://github.com/AlessandroSaullo/OverEncryptionSwift.git 
-sudo git push origin master
+sudo git push origin devAle 
