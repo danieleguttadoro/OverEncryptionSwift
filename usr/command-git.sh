58e5addb3e5cb80a747a10d817a6bc69a4dbb052
AlessandroSaullo <alessandro.saullo1@gmail.com>
2015-12-17 16:33:01 +0100

    Update and rename command-git.sh to commit.sh

--- usr/command-git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo git add *
-echo "Insert commit message"
-read msg
-sudo git commit -m msg
-sudo git remote add origin https://github.com/AlessandroSaullo/OverEncryptionSwift.git 
-echo "Insert branch"
-read branch
-sudo git push origin branch 
