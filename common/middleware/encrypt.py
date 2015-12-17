df8d248694c3d61339e8f6a029a7b77c4318ed72
AlessandroSaullo <alessandro.saullo1@gmail.com>
2015-12-17 17:27:18 +0100

    Create encrypt.py

---
+++ common/middleware/encrypt.py
@@ -0,0 +1,24 @@
+from swift import gettext_ as _
+
+from swift.common.swob import Request, HTTPServerError
+from swift.common.utils import get_logger, generate_trans_id
+from swift.common.wsgi import WSGIContext
+from swift.proxy.controllers.container import ContainerController
+from swift.proxy.controllers.base import get_container_info
+class encrypt(WSGIContext):
+
+   def __init__(self,app, conf):
+        self.app = app
+        self.conf = conf
+
+   def __call__(self, env, start_response):
+        print "-----------------ENCRYPT -----------------------"
+
+
+def filter_factory(global_conf, **local_conf):
+    conf = global_conf.copy()
+    conf.update(local_conf)
+
+    def except_filter(app):
+        return encrypt(app,conf)
+    return except_filter
