8920c782981a46d0a3db10317eea5939af2a31e8
AlessandroSaullo <alessandro.saullo1@gmail.com>
2015-12-17 17:40:11 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Choose branch: master, devAle, devDan"
 read branch
 cd /home/stack/swift/
 sudo git checkout $branch
-sudo cp /home/stack/swift/common/middleware/overencrypt.py /opt/stack/swift/swift/common/middleware
+sudo cp /home/stack/swift/common/middleware/encrypt.py /opt/stack/swift/swift/common/middleware
+sudo cp /home/stack/swift/common/middleware/decrypt.py /opt/stack/swift/swift/common/middleware
+sudo cp /home/stack/swift/common/middleware/key_master.py /opt/stack/swift/swift/common/middleware
 cat /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/entry_points.txt | sudo tee /opt/stack/swift/swift.egg-info/entry_points.txt
 cat /home/stack/swift/usr/stow/etc/swift/proxy-server.conf | sudo tee /etc/swift/proxy-server.conf
