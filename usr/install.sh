5b944d58ae6dd2ed9aff0165f597619f6573320e
AlessandroSaullo <alessandro.saullo1@gmail.com>
2016-01-19 16:04:21 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -2,6 +2,7 @@
 sudo cp /home/stack/swift/common/middleware/encrypt.py /opt/stack/swift/swift/common/middleware
 sudo cp /home/stack/swift/common/middleware/decrypt.py /opt/stack/swift/swift/common/middleware
 sudo cp /home/stack/swift/common/middleware/key_master.py /opt/stack/swift/swift/common/middleware
+sudo cp /home/stack/swift/common/middleware/catalog_functions.py /opt/stack/swift/swift/common/middleware
 #sudo pip install pika
 #sudo cp /home/stack/swift/usr/stow/home/vagrant/.vimrc /home/vagrant/
 #cat /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/entry_points.txt | sudo tee /opt/stack/swift/swift.egg-info/entry_points.txt
