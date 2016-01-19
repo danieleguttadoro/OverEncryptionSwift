19879532f5f2789526859b6bb7dedf95d407ceca
AlessandroSaullo <alessandro.saullo1@gmail.com>
2016-01-19 19:05:03 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -3,10 +3,6 @@ sudo cp /home/stack/swift/common/middleware/encrypt.py /opt/stack/swift/swift/co
 sudo cp /home/stack/swift/common/middleware/decrypt.py /opt/stack/swift/swift/common/middleware
 sudo cp /home/stack/swift/common/middleware/key_master.py /opt/stack/swift/swift/common/middleware
 sudo cp /home/stack/swift/common/middleware/catalog_functions.py /opt/stack/swift/swift/common/middleware
-sudo mkdir -m 755 -p /opt/stack/sel-daemon/
-sudo mkdir -m 755 -p /opt/stack/sel-daemon/config
-sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/daemon.py /opt/stack/sel-daemon/
-sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/config/sel-daemon.ini /opt/stack/sel-daemon/config/
 #sudo pip install pika
 #sudo cp /home/stack/swift/usr/stow/home/vagrant/.vimrc /home/vagrant/
 #cat /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/entry_points.txt | sudo tee /opt/stack/swift/swift.egg-info/entry_points.txt
