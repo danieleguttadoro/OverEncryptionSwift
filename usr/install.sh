cfebd6c7e97d88e8f93ad6cd427d3ad959879439
danieleguttadoro <daniele.guttadoro@gmail.com>
2016-01-20 12:10:57 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -6,6 +6,6 @@ sudo cp /home/stack/swift/usr/stow/home/vagrant/.vimrc /home/vagrant/
 sudo mkdir -m 755 -p /opt/stack/sel-daemon/
 sudo mkdir -m 755 -p /opt/stack/sel-daemon/config
 sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/daemon.py /opt/stack/sel-daemon/
-sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/config/sel-daemon.ini /opt/stack/sel-daemon/config/
+sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/config/daemon.ini /opt/stack/sel-daemon/config/
 #cat /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/entry_points.txt | sudo tee /opt/stack/swift/swift.egg-info/entry_points.txt
 #cat /home/stack/swift/usr/stow/etc/swift/proxy-server.conf | sudo tee /etc/swift/proxy-server.conf
