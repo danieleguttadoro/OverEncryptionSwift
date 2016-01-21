2e0674689fb71d04ca85b2a5ee993426b60c5a2e
root <root@manager.openstack.site>
2016-01-21 12:08:49 +0000

    Add_Multiprocessing_daemon_(stable_version)

--- usr/stow/home/stack/sel-daemon/daemon.py
+++ usr/stow/home/stack/sel-daemon/daemon.py
@@ -1,4 +1,4 @@
-import os, sys, psutil
+import os, sys, psutil, time, pika
 from multiprocessing import Process, Pipe
 
 #def control_task(conn):
@@ -7,9 +7,27 @@ from multiprocessing import Process, Pipe
 #print('Value [%d] sent by PID [%d]' % (value, os.getpid()))
 #conn.close()
 
-def consumer_task(conn):
-print('Value [%d] received by PID [%d]' % (conn.recv(),
-os.getpid()))
+def consumer_task():
+
+    while (True):
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='hello', durable=True)
+            
+        def callback(ch, method, properties, body):
+            print(" [x] Received %r" % body)
+            time.sleep(body.count(b'.'))
+            print(" [x] Done")
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+        
+        channel.basic_qos(prefetch_count=1)    
+        channel.basic_consume(callback,
+                      queue='hello')
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
 
 if __name__ == '__main__':
     #producer_conn, consumer_conn = Pipe()
@@ -19,42 +37,58 @@ if __name__ == '__main__':
     #control.start()
     #consumer.join()
     #control.join()
+
+    n_ini = 8
     ctrl_list = []
     
-    for i in range(1,9):
-        consumer = Process(target=consumer_task))
-        ctrl_list.append(os.getpid())
-        consumer.start()
-    
+    for i in range(1,n_ini+1):
+        pid = os.fork()
+        if pid:
+            ctrl_list.append(pid)
+        else: consumer_task()
+
     while(True):
 
-        count_idle = 0
+        count_sleep = 0
 
         for pid in ctrl_list:
              proc = psutil.Process(pid)
-             if proc.status() == psutil.STATUS_IDLE:
-                count_idle += 1
-             elif proc.status() == psutil.STATUS_RUNNING:
-                count_idle -= 1
-             else sys.stderr.write('Error on Process Status')
+             print(' PID [%d] status [%s]' % (pid, proc.status))
+             if proc.status == psutil.STATUS_SLEEPING:
+                count_sleep += 1
+             elif proc.status == psutil.STATUS_RUNNING:
+                count_sleep -= 1
+             else: sys.stderr.write('Error on Process Status')
 
         ctrlen = len(ctrl_list)
         threshold = 3/4*ctrlen
 
-        if count_idle > threshold:
-            for pid in ctrl_list[threshold:]
+        if (count_sleep > threshold and count_sleep > n_ini):
+            i = 0
+            for pid in ctrl_list:
+                if i >= ctrlen/4:
+                    break
                 proc = psutil.Process(pid)
-                proc.kill()
-            ctrl_list = ctrl_list[:threshold-1]
-        elif 2*abs(count_idle) > ctrlen
+                if proc.status == psutil.STATUS_SLEEPING:
+                    proc.kill()
+                    i += 1
+                    ctrl_list.remove(pid)
+                    print ('             **** DELETED IDLE [%d] ****' % (pid))
+        elif -count_sleep > ctrlen*3/4:
             for i in range (1,ctrlen/2):        
-                consumer = Process(target=consumer_task))
-                ctrl_list.append(os.getpid())
-                consumer.start()
+                pid = os.fork()
+                if pid:
+                    ctrl_list.append(pid)
+                else: consumer_task()
+                print ('             **** CREATED HALF [%d] ****' % (pid))
+        elif -count_sleep > ctrlen/2:
+            for i in range (1,ctrlen/3):        
+                pid = os.fork()
+                if pid:
+                    ctrl_list.append(pid)
+                else: consumer_task()
+                print ('             **** CREATED THIRD [%d] ****' % (pid))
 
-
-
+        time.sleep(3)
 
     #never reached??
-    #for i in consumers
-    #    i.join()
