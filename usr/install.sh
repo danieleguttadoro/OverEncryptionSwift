12e27bc4d90700b1a653c5674c7c1b4807181511
AlessandroSaullo <alessandro.saullo1@gmail.com>
2016-02-01 19:03:52 +0100

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -12,6 +12,7 @@ sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/daemon.py /opt/stack/se
 #sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/config/daemon.ini /opt/stack/sel-daemon/config/
 #sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/log/daemon.log /opt/stack/sel-daemon/log/
 sudo chmod +x /opt/stack/sel-daemon/daemon.py
+sudo pip install --upgrade pip
 sudo pip install pika
 #cat /home/stack/swift/usr/stow/opt/stack/swift/swift.egg-info/entry_points.txt | sudo tee /opt/stack/swift/swift.egg-info/entry_points.txt
 #cat /home/stack/swift/usr/stow/etc/swift/proxy-server.conf | sudo tee /etc/swift/proxy-server.conf
