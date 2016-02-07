d83591dc09504b68a7525fadab7b036e42808894
root <root@manager.openstack.site>
2016-02-07 17:38:07 +0000

    encryption,key_master_refactoring

--- common/middleware/encrypt.py
+++ common/middleware/encrypt.py
@@ -1,116 +1,46 @@
 from swift import gettext_ as _
-
-
-from swift.common.swob import Request, HTTPServerError
+import md5
+import time
+from swift.common.swob import Request, HTTPServerError, wsgify
 from swift.common.utils import get_logger, generate_trans_id
 from swift.common.wsgi import WSGIContext
-from swift.container.server import ContainerController
-import os, time, psutil
-from swift.common.http import is_success
-from swift.common.swob import wsgify
+from swift.proxy.controllers.container import ContainerController
+from swift.proxy.controllers.base import get_container_info
+import catalog_functions
 import crypto_functions as cyf
 
-class encrypt(WSGIContext):
+class decrypt(WSGIContext):
 
-    def __init__(self,app, conf):
+   def __init__(self,app, conf):
         self.app = app
         self.conf = conf
 
-    def __call__(self, env, start_response):
+   @wsgify
+   def __call__(self, req):
+        
         print "----------------- ENCRYPT -----------------------"
-        req = Request(env)
-        #print req.method
         
-        username   = env.get('HTTP_X_USER_NAME',None)
-        #if is_success(resp.status_int):
-        if req.method == "POST" and username != "admin" and username != None:
-            
-            old_cryptotoken = env.get('swift_crypto_fetch_old_crypto_token',None)
-            new_token = env.get('swift_crypto_fetch_new_token',None)
-
-            if new_token != None:
-                version, account, container, obj = req.split_path(1,4,True)
-            
-                new_req = req
-                new_req = Request.blank(req.path_info,None,req.headers,None)
-                new_req.method = 'GET'
-                get_resp = new_req.get_response(self.app)
-                
-                cryptokey = key = get_resp.headers.get('X-Container-Sysmeta-Crypto-Key',None)
-
-                if old_cryptotoken != None:
-                    old_token = cyf.decrypt_resource(old_cryptotoken,cyf.get_privatekey())
-                    print old_token
-                    print len(old_token)
-                    key = cyf.decrypt_resource(cryptokey,old_token)
-                    #Da aggiungere last_modified e content length
-                elif cryptokey == None: 
-                    key = cyf.gen_key()
-                    list_file = get_resp.body.split('\n')[:-1]
-                
-                    #PUT di un nuovo container con il nome criptato
-                    #new_headers.method = "PUT"
-                    #cryptocontainer = cyf.encrypt_resource(container,key)
-                    #new_path_info = "/".join(version,account,cryptocontainer)
-                    #new_req_put = Request.blank(new_path_info,None,new_headers,None)
-                
-                    #sons_list = []
-                    for obj in list_file: 
-                        pid = os.fork()
-                        if not pid:
-            
-                            #sons_list.append(pid)
-                        #else:
-                            print " I AM THE SON! " + obj
-                            # get file dalocntainer vecchio, cripto il file, faccio la put del file nel container (per ora lo stesso) e muoio
-                            new_path_info = "/".join(["",version,account,container,obj])
-                            #print "____________________________________________________"
-                            #print new_path_info 
-                            new_req = Request.blank(new_path_info,None,req.headers,None)
-                            new_req.method = 'GET'
-                            get_resp = new_req.get_response(self.app)
-                            print "CLEAR BODY " + obj
-                            print get_resp.body
-                            cryptobody = cyf.encrypt_resource(get_resp.body,key)
-                            #print "ENC BODY "+ obj
-                            #print cryptobody
-                            new_req.body = cryptobody
-                            new_req.method = 'PUT'
-                            #print new_req_put.method
-                            #print new_req_put.headers
-                            put_resp = new_req.get_response(self.app)
-                        
-                            proc = psutil.Process(os.getpid())
-                            #sons_list.remove(os.getpid()) #il figlio qui non vede la stessa lista del padre, infatti, e' sollevato un errore sulla remove 'pid non trovato'
-                            #print " I KILL MESELF "+ obj
-                            proc.kill()
-
-                    #Creo un fratellastro che aspetti tutti i processi che hanno criptato gli oggetti, e cancella il vecchio container
-                    #pid = os.fork()
-                    #if not pid:
-                    #    while sons_list:
-                            # cicla finche la lista non e' vuota. 
-                            # In python-style-guide: per python (strings, lists, tuples) empty are false
-                            #time.sleep(3)
-                        #lista vuota
-                        #cancella il vecchio container e muori
-                    
-                cryptokey = cyf.encrypt_resource(key,new_token)
-                
-                new_headers = req.headers
-                new_headers['X-Container-Sysmeta-Crypto-Key'] = cryptokey
-                new_req = Request.blank(req.path_info,None,new_headers,None)
-                new_req.method = 'POST'
-                post_resp = new_req.get_response(self.app)
-                    
-                        
-        return self.app(env, start_response)       
+        resp = req.get_response(self.app)
+        env = req.environ
+        username = env.get('HTTP_X_USER_NAME',None)
         
+        if req.method == "GET" and username != 'admin' and username != None:               
+            
+            token = req.environ.get('swift_crypto_fetch_token',None)       
+            
+            if token != None:
+                resp.body = cyf.decrypt_resource(resp.body,token) 
+                resp.headers['Etag'] = md5.new(resp.body).hexdigest()
+                #last_modified = cyf.decrypt_resource(resp.last_modified,token)
+                resp.content_lenght = len(resp.body)  
+        
+        return resp 
+         
 
 def filter_factory(global_conf, **local_conf):
     conf = global_conf.copy()
     conf.update(local_conf)
 
     def except_filter(app):
-        return encrypt(app,conf)
+        return decrypt(app,conf)
     return except_filter
--- common/middleware/key_master.py
+++ common/middleware/key_master.py
@@ -9,33 +9,10 @@ from swift.common.swob import wsgify
 import catalog_functions
 import crypto_functions as cyf
 import time
-#To use barbican
-import sys
-import traceback
 
-#from barbicanclient import client
 from keystoneclient import session
-from keystoneclient.auth import identity
-################
 
-#def acl(obj):
-#    if obj == 'object1':
-#        return ['142de5b2c87b4827861c536059c97c37','b48877a174e641fc91dd85540e8643b9']
-#    elif obj == 'object2':
-#        return ['142de5b2c87b4827861c536059c97c37']
-
-def to_do_overencryption():
-    # for now, random function to establish if we must do an overencryption. Later, we scan BEL-graph.
-    #return random.randint(0,1)
-    return 1
-
-def create_head_req(req):
-    version, account, container, obj = req.split_path(1,4,True)
-    path_info = "/".join(["",version,account,container])
-    new_head_req = Request.blank(path_info,None,req.headers,None)
-    new_head_req.method = 'HEAD'
-    return new_head_req
-
+DAEMON_IP = '127.0.0.1'
 class key_master(WSGIContext):
 
     def __init__(self,app, conf):
@@ -46,180 +23,69 @@ class key_master(WSGIContext):
         
         print "----------------- KEY_MASTER -----------------------"
         req = Request(env)
-        #barbican_client()
-        #COMMENT: Finding user and token
+        
         username   = env.get('HTTP_X_USER_NAME',None)
         userid     = env.get('HTTP_X_USER_ID',None)
-        auth_token = env.get('HTTP_X_AUTH_TOKEN',None)
         print "USERNAME ----> ", username
         print "USERID   ----> ", userid
+        
         #COMMENT: Control the author of the request. DA AGGIUNGERE IL CONTROLLO SULL'ID DEL CEILOMETER(OMONOMIA con un utente)
-        if username != "ceilometer" and username != "admin" and username != None and req.method != 'PUT':
-            container = req.split_path(1,4,True)[2]
-	        #Get the catalog from metacontainer
-            found_meta_container, json_catalog = catalog_functions.get_catalog(self.app,auth_token,req,userid,username)        
-            if req.method == "GET" and json_catalog != None and found_meta_container != None:
-                #COMMENT: if json_catalog == None, overencrypt never done
-                #COMMENT: if found_meta_container == None, not exist a catalog	
-                #COMMENT: Scan the graph to obtain the key and insert it in the env (GET) or to modify the graph in order to add or delete a key (PUT)
-                graph =  catalog_functions.load_graph(json_catalog)
-                cryptotoken = catalog_functions.get_cryptotoken(graph,container) 
-                if cryptotoken != None:
-	                #COMMENT: Setto il cryptotoken per renderlo disponibile al decrypt
-                    #env['swift_crypto_fetch_crypto_token'] = "XZmBROlPFEZbfTn0PPGKVwen5MCct56FtOM/XFaDg62DEW85MOhYz+8KXiwj15itgxFvOTDoWM/vCl4sI9eYrQ=="
-                    env['swift_crypto_fetch_crypto_token'] = cryptotoken
-                    #COMMENT: Richiedo la cryptokey relativa al container e la inserisco nell'environ per il decrypt
-                head_req = create_head_req(req)
-                head_resp = head_req.get_response(self.app)
-                #env['swift_crypto_fetch_crypto_key'] = "lFcV0dBnmWXzrJh2WaME4wen5MCct56FtOM/XFaDg62DEW85MOhYz+8KXiwj15itgxFvOTDoWM/vCl4sI9eYrQ=="
-                env['swift_crypto_fetch_crypto_key'] = head_resp.headers.get('x-container-sysmeta-crypto-key',None)
-                    	     
-            elif req.method== "POST":
-                if to_do_overencryption():         
-                    #LISTA ABC DA RICAVARE DALLA MODIFICA DELLA ACL O DA OVERENCRYPT
-                    token = cyf.gen_token()
-                    node = catalog_functions.create_node(container,req.headers.get('x-container-meta-acl-label',None),
-                                                            token,userid)
-                    catalog_functions.send_message("INSERT",userid,node)
-                    #Encrypt the resource
-                    if json_catalog != None:
-                        graph =  catalog_functions.load_graph(json_catalog)
-                        old_cryptotoken = catalog_functions.get_cryptotoken(graph,container)
-                        env['swift_crypto_fetch_old_crypto_token'] = old_cryptotoken
-                    env['swift_crypto_fetch_new_token'] = token
-                else:
-                    if json_catalog != None:
-                        graph = catalog_functions.load_graph(json_catalog)
-                        node = catalog_functions.get_node(graph,container)
-                        if node != None:
-                            catalog_functions.send_message("REMOVE",userid,node)
-                            cryptotoken = node['CRYPTOTOKEN']
-                            #print ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
-                            #print cryptotoken
-                            token = cyf.decrypt_resource(cryptotoken,cyf.get_privatekey())
-                            #print ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
-                            #print token
-                            new_req = create_head_req(req)
-                            head_resp = new_req.get_response(self.app)
-                            cryptokey = head_resp.headers.get('x-container-sysmeta-crypto-key',None)
-                            #print ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
-                            #print cryptokey
-                            key = cyf.decrypt_resource(cryptokey,token)
-                            #print ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
-                            #print key
-                            #time.sleep(8)
-                            new_req.headers['x-container-sysmeta-crypto-key'] = key
-                            new_req.method = 'POST'
-                            new_req.get_response(self.app)
-                            		       
-                                
-            elif req.method == "DELETE":
-	            #TODO
-      
-	            pass
+        if req.method == "GET" and username != "ceilometer" and  username != "admin" and username != None:
+            
+            version, account, container, obj = req.split_path(1,4,True)
+            send_message(userid,container)
+            receive_message(userid)	        
         
         return self.app(env, start_response)
 
+def receive_message(userid):
 
-def barbican_client():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(DAEMON_IP))
+    channel = connection.channel()
+    channel.queue_declare(queue='swift_'+userid, durable=True)
+           
+    channel.basic_qos(prefetch_count=1)    
+    channel.basic_consume(receive_message,
+                      queue='swift_'+userid)
 
-    #Variables for barbican
-    ADMIN_USER="admin"
-    ADMIN_PASS="secretsecret"
-    ADMIN_TENANT="demo"
-    AUTH_URL="http://127.0.0.1:5000/v2.0"
-    BARB_URL="http://127.0.0.1:9311"
-    AUTH_IP="127.0.0.1"
-	
-    try:
-        # We'll use Keystone API v3 for authentication
-        auth = identity.v2.Password(auth_url=AUTH_URL,
-            username=ADMIN_USER,
-            password=ADMIN_PASS,
-            tenant_name=ADMIN_TENANT)
+    print(' Waiting for messages [%d] ...' % userid)
+    channel.start_consuming()
 
-        # Next we'll create a Keystone session using the auth plugin we just created
-        sess = session.Session(auth=auth)
+def receive_message(ch, method, properties, body):
+        
+    #COMMENT: Setto il cryptotoken per renderlo disponibile all' encrypt
+    if body != None:
+        token = cyf.decrypt_resource(body,cyf.get_privatekey())
+    else: 
+        token = None
+    env['swift_crypto_fetch_token'] = token
 
-        print '-----------------------BARBICAN---------------------------'
-        print 'session created successfully'
+    return
 
-        # Now we use the session to create a Barbican client
-        barbican = client.Client(endpoint=BARB_URL, session=sess)
+def send_message(userid,container):
+                
+    connection = pika.BlockingConnection(pika.ConnectionParameters(DAEMON_IP))
+    channel = connection.channel()
+    channel.queue_declare(queue='daemon_get_swift', durable=True)
+       
+    channel.confirm_delivery()
+    msg = 'GET_CRYPTOTOKEN' + '#' + userid + '#' + container
 
-        print 'barbican client created successfully'
+    try:  
+        channel.basic_publish(exchange='',
+                              routing_key='daemon_get_swift',
+                              body=msg,
+                              properties=pika.BasicProperties(
+                              delivery_mode = 2, # make message persistent
+                              ))
+        
+        print(" Sent [%s]" % msg)
+    except pika.exceptions.ConnectionClosed as exc:
+        print('Error. Connection closed, and the message was never delivered.')
 
-        # Let's create a Secret to store some sensitive data
-        secret = barbican.secrets.create(name=u'Self destruction sequence',
-                                         payload=u'the magic words are squeamish ossifrage')
+    connection.close()
 
-        print 'secret created successfully'
-
-        # Now let's store the secret by using its store() method. This will send the secret data
-        # to Barbican, where it will be encrypted and stored securely in the cloud.
-        secret.store()
-        #u'http://localhost:9311/v1/secrets/85b220fd-f414-483f-94e4-2f422480f655'
-
-        print 'secret stored successfully'
-
-        # The URI returned by store() uniquely identifies your secret in the Barbican service.
-        # After a secret is stored, the URI is also available by accessing
-        # the secret_ref attribute.
-        print(secret.secret_ref.replace('localhost',AUTH_IP))
-        #http://localhost:9311/v1/secrets/091adb32-4050-4980-8558-90833c531413
-
-        # When we need to retrieve our secret at a later time, we can use the secret_ref
-        retrieved_secret = barbican.secrets.get(secret.secret_ref.replace('localhost',
-            AUTH_IP))
-        # We can access the secret payload by using the payload attribute.
-        # Barbican decrypts the secret and sends it back.
-        print(retrieved_secret.payload)
-        #the magic words are squeamish ossifrage
-
-    except:
-        traceback.print_exc(file=sys.stdout)
-"""        
-        req = Request(env)
-        resp = req.get_response(self.app)
-
-        #COMMENT: Finding user and method
-        username = env.get('HTTP_X_USER_NAME',None)
-        #COMMENT: Control the author of the request. DA AGGIUNGERE IL CONTROLLO SULL'ID DEL CEILOMETER(OMONOMIA con un utente)
-        if username != "ceilometer":
-
-            #COMMENT: Obtaining version and account of the Request, to do another Request and obtain the graph of tokens 
-	        version, account, container, obj = req.split_path(1,4,True)  
-	        path_meta = "/".join(["", version , account , self.meta_container, self.graph_tokens])  
-	        print path_meta
-	        req_meta_container = Request.blank(path_meta,None,req.headers,None)
-	        req_graph = req_meta_container.get_response(self.app)
-	        print req_graph.body
-	     
-        	# COMMENT: Scan the graph to obtain the key and insert it in the env (GET) or to modify the graph in order to add or delete a key (PUT)
-            # Example: retrieve the key 
-            print "Retrieve the key ..."
-            key = '01234567890123456789012345678901' # 32 char length
-
-            if key == '':
-                raise_error(req,403)
-            env['swift_crypto_fetch_crypto_key'] = key
-
-            #COMMENT: Modify the graph
-            #Fake modify of the graph
-            req_meta_container.body = "Modifica effettuata"
-
-            #COMMENT: Upload on metacontainer the new version of graph
-            req_meta_container.method = 'PUT'
-            req_meta_container.get_response(self.app)
-
-@wsgify
-def raise_error(req,stat):
-
-    if stat == 403:
-        return Response(request=req, status=403, body="USER UNAUTHORIZED TO OBTAIN THIS FILE",
-                        content_type="text/plain")
-
-"""
+    return
 
 def filter_factory(global_conf, **local_conf):
     conf = global_conf.copy()
