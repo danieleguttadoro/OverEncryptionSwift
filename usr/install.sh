7ade947cad23b48c3e22f86e74fa14fc5dcfb522
danieleguttadoro <danieleguttadoro@users.noreply.github.com>
2016-06-09 10:30:53 +0200

    Update install.sh

--- usr/install.sh
+++ usr/install.sh
@@ -13,8 +13,9 @@ sudo mkdir -m 755 -p /opt/stack/sel-daemon/
 #sudo mkdir -m 755 -p /opt/stack/sel-daemon/log
 
 sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/daemon.py /opt/stack/sel-daemon/
-sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/rabbit_connection.py /opt/stack/sel-daemon/
-sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/receive_message.py /opt/stack/sel-daemon/
+sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/catalogue.py /opt/stack/sel-daemon/
+sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/connection.py /opt/stack/sel-daemon/
+sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/crypto_functions.py /opt/stack/sel-daemon/
 #sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/config/daemon.ini /opt/stack/sel-daemon/config/
 #sudo cp /home/stack/swift/usr/stow/home/stack/sel-daemon/log/daemon.log /opt/stack/sel-daemon/log/
 
