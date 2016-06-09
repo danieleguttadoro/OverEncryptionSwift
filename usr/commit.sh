98e1b26946a16eb58fc47d4da86548f45ea972c4
danieleguttadoro <danieleguttadoro@users.noreply.github.com>
2016-06-09 10:17:35 +0200

    Update commit.sh

--- usr/commit.sh
+++ usr/commit.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 cd /home/stack/swift/
+
 echo "Insert branch to commit: master"
+
 read branch
 sudo git checkout $branch
+
 cat /opt/stack/swift/swift/common/middleware/key_master.py | sudo tee /home/stack/swift/common/middleware/key_master.py
 cat /opt/stack/swift/swift/common/middleware/encrypt.py | sudo tee /home/stack/swift/common/middleware/encrypt.py
 cat /opt/stack/swift/swift/common/middleware/catalogue.py | sudo tee /home/stack/swift/common/middleware/catalogue.py
 cat /opt/stack/swift/swift/common/middleware/crypto_functions.py | sudo tee /home/stack/swift/common/middleware/crypto_functions.py
 cat /opt/stack/swift/swift/common/middleware/connection.py | sudo tee /home/stack/swift/common/middleware/connection.py
+
+#cat /etc/swift/proxy-server.conf  | sudo tee /home/stack/swift/usr/stow/etc/swift/proxy-server.conf
+
 cat /opt/stack/sel-daemon/receive_message.py | sudo tee /home/stack/swift/usr/stow/home/stack/sel-daemon/receive_message.py
 cat /opt/stack/sel-daemon/rabbit_connection.py | sudo tee /home/stack/swift/usr/stow/home/stack/sel-daemon/rabbit_connection.py
-#cat /etc/swift/proxy-server.conf  | sudo tee /home/stack/swift/usr/stow/etc/swift/proxy-server.conf
 cat /opt/stack/sel-daemon/daemon.py | sudo tee /home/stack/swift/usr/stow/home/stack/sel-daemon/daemon.py
+
 sudo git add *
-echo "Insert message for commit:"
+
+echo "Insert message for commit (space with _):"
 read -e msg
+
 sudo git commit -m $msg
 sudo git push origin $branch
