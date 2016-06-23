3cd3ef429b632d05d77b8d31fece4695552723f8
root <root@manager.openstack.site>
2016-06-23 15:57:38 +0000

    Included_PKCS1_OAEP

--- common/middleware/key_manager.py
+++ common/middleware/key_manager.py
@@ -3,9 +3,9 @@
 import os
 import base64
 from Crypto import Random
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES,PKCS1_OAEP
 from Crypto.Signature import PKCS1_PSS
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA256
 #from Crypto.PublicKey import RSA
 import imp
 from connection import *
@@ -28,59 +28,35 @@ def generate_container_key():
 
     return id_,secret
 
-
-def encrypt_DEK(secret, sender, receiver):
-        """
-        Cipher the DEK for the catalog using either AES or RSA encryption
-        """
-        # sender = self.userID
-        if sender == receiver:
-            # AES encryption using the master key
-            master_key = get_masterKey()
-            pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-            secret = pad(secret)
-            iv = Random.new().read(AES.block_size)
-            cipher = AES.new(master_key, AES.MODE_CBC, iv)
-            result = base64.b64encode(iv + cipher.encrypt(secret))
-        else:
-            # RSA encryption using the sender's private key and the receiver's public one
-            receiver_pub_key = RSA.importKey(get_publicKey(receiver))
-            #receiver_pub_key= PKCS1_OAEP.new(receiver_pub_key_rsa)
-            sender_priv_key = RSA.importKey(get_privateKey())
-            #sender_priv_key = PKCS1_OAEP.new(sender_priv_key_rsa)
-            #ciph1 = receiver_pub_key.decrypt(secret)
-            ciph1 = receiver_pub_key.encrypt(secret,'x')[0]
-            h = SHA.new()
-            h.update(ciph1)
-            signer = PKCS1_PSS.new(sender_priv_key)
-            signature = signer.sign(h)
-            result = str(base64.b64encode(ciph1)) + "#" + str(base64.b64encode(signature))
-            print result
-        return result
-
-
 def decrypt_KEK(secret,signature, sender, receiver):
         """
         Decipher the KEK from the catalog.
+        
         Returns:
             Dek
         """
+        sender_pub_key = RSA.importKey(get_publicKey(sender))
         # receiver = self.userID
         if sender == receiver:
             # AES decipher
-            master_key = get_masterKey()
-            unpad = lambda s: s[: -ord(s[len(s) - 1:])]
-            secret = base64.b64decode(secret)
-            iv = secret[:BLOCK_SIZE]
-            cipher = AES.new(master_key, AES.MODE_CBC, iv)
-            result = unpad(cipher.decrypt(secret[BLOCK_SIZE:]))
+            h = SHA256.new()
+            h.update(secret)
+            verifier = PKCS1_PSS.new(sender_pub_key)
+            if verifier.verify(h,signature):
+                master_key = get_masterKey()
+                unpad = lambda s: s[: -ord(s[len(s) - 1:])]
+                secret = base64.b64decode(secret)
+                iv = secret[:BLOCK_SIZE]
+                cipher = AES.new(master_key, AES.MODE_CBC, iv)
+                result = unpad(cipher.decrypt(secret[BLOCK_SIZE:]))
+            else:
+                #error
+                pass
         else:
             # RSA decipher
-            sender_pub_key = RSA.importKey(get_publicKey(sender))
-            receiver_priv_key = RSA.importKey(get_privateKey())
-            #sender_pub_key = PKCS1_OAEP.new(sender_pub_key_rsa)
-            #receiver_priv_key = PKCS1_OAEP.new(receiver_priv_key_rsa)
-            h = SHA.new()
+            receiver_priv_key_rsa = RSA.importKey(get_privateKey())
+            receiver_priv_key = PKCS1_OAEP.new(receiver_priv_key_rsa)
+            h = SHA256.new()
             h.update(secret)
             verifier = PKCS1_PSS.new(sender_pub_key)
             if verifier.verify(h,signature):
@@ -99,26 +75,13 @@ def encrypt_msg(info, secret, path=False):
     # padding : guarantee that the value is always MULTIPLE  of BLOCK_SIZE
     PADDING = '{'
     pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-    encodeAES = lambda c, s: (c.encrypt(pad(s)))
+    encodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
     cipher = AES.new(secret)
     encoded = encodeAES(cipher, info)
     if path:
         # Encoding base32 to avoid paths (names containing slashes /)
-        encoded = (encoded)
+        encoded = base64.b32encode(encoded)
     return encoded
-
-def decrypt_msg(encryptedString, secret, path=False):
-    """
-    Decrypt a message using AES
-    """
-    PADDING = '{'
-    if path:
-        encryptedString = (encryptedString)
-    decodeAES = lambda c, e: c.decrypt((e)).rstrip(PADDING)
-    key = secret
-    cipher = AES.new(key)
-    decoded = decodeAES(cipher, encryptedString)
-    return decoded
 
 def get_masterKey():    
     """ 
